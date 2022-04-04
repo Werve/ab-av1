cc05670ed05cab5dfb1933f3e8957758151db81a
Werve <13850028+Werve@users.noreply.github.com>
2022-04-04 17:36:50 +0200

    Update Windows_cfr_search.bat

--- Windows_cfr_search.bat
+++ Windows_cfr_search.bat
@@ -5,26 +5,25 @@ IF "%_file%" == "" (GOTO InputRequired)
 FOR /F "tokens=1 USEBACKQ" %%F IN (`where ab-av1`) DO (
 	SET "_abav1path=%%F"
 )
-REM echo %_abav1path%
+
 IF "%_abav1path%" == "" (GOTO abav1Miss)
 SET "_abav1dir=%_abav1path:ab-av1.exe=%"
-echo %_abav1dir%
+
 FOR /F "tokens=1 USEBACKQ" %%F IN (`where /r %_abav1dir% vmaf_v?.?.?.json`) DO (
 	SET "_vmafmodel=%%F"
 )
-REM echo %_vmafmodel%
+
 IF "%_vmafmodel%" == "" (GOTO VmafModelRequired)
 
 SET "_linuxvmafmodel=%_vmafmodel:\=/%"
-REM echo %_linuxvmafmodel%
-CALL SET "_linuxvmafmodelR=%%_linuxvmafmodel::=\:%%"
-REM echo %_linuxvmafmodelR%
 
-REM echo %_file%
-REM GOTO InputRequired
+CALL SET "_linuxvmafmodelR=%%_linuxvmafmodel::=\:%%"
+
+
+SET /P "_arg=If you want to pass an argument to ab-av1 write now (like: --min-vmaf <MIN_VMAF>)"
 FOR %%G IN (12 10 8 6 4) DO (
 	echo =^> preset %%G
-	ab-av1 crf-search -i "%_file%" --preset %%G --vmaf '"model_path=%_linuxvmafmodelR%"'
+	ab-av1 crf-search -i "%_file%" --preset %%G --vmaf '"model_path=%_linuxvmafmodelR%"' %_arg%
 )
 pause
 exit /b
